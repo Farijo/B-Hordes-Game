aa77add5598d42602fe39226b131405a7775d9b6
tvallar <teddy.vallar@dazz.tv>
2024-02-27 18:45:55 +0100

    sql data

---
+++ db/description.sql
@@ -0,0 +1,80 @@
+USE hordes_challenge;
+
+DROP TABLE IF EXISTS invitation;
+DROP TABLE IF EXISTS milestone;
+DROP TABLE IF EXISTS success;
+DROP TABLE IF EXISTS validator;
+DROP TABLE IF EXISTS goal;
+DROP TABLE IF EXISTS participant;
+DROP TABLE IF EXISTS challenge;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    simplified_name VARCHAR(30) NOT NULL,
+    avatar VARCHAR(255)
+);
+CREATE TABLE challenge (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    creator INT NOT NULL,
+    flags TINYINT UNSIGNED NOT NULL, -- & 0x03 = participation (open, request, invite), & 0x04 = private, & 0x08 = no api validation, & 0x30 = status (draft, review, invite)
+    start_date DATETIME,
+    end_date DATETIME,
+    FOREIGN KEY(creator) REFERENCES user(id)
+);
+CREATE TABLE participant (
+    user INT NOT NULL,
+    challenge INTEGER NOT NULL,
+    PRIMARY KEY(user,challenge),
+    FOREIGN KEY(challenge) REFERENCES challenge(id)
+);
+CREATE TABLE goal (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    challenge INTEGER NOT NULL,
+    typ TINYINT NOT NULL,
+    descript VARCHAR(255),
+    FOREIGN KEY(challenge) REFERENCES challenge(id)
+);
+CREATE TABLE validator (
+    user INT NOT NULL,
+    challenge INTEGER NOT NULL,
+    PRIMARY KEY(user,challenge),
+    FOREIGN KEY(challenge) REFERENCES challenge(id)
+);
+CREATE TABLE success (
+    user INT NOT NULL,
+    goal INTEGER NOT NULL,
+    accomplished DATETIME(2) NOT NULL,
+    amount INT NOT NULL,
+    PRIMARY KEY(user,goal,amount),
+    FOREIGN KEY(goal) REFERENCES goal(id),
+    FOREIGN KEY(user) REFERENCES user(id)
+);
+CREATE TABLE milestone (
+    user INT NOT NULL,
+    dt DATETIME(2) NOT NULL,
+    isGhost BOOLEAN,
+    playedMaps INT,
+    rewards BLOB, -- BLOB = concatenation de uint16, uint32 (id du picto, nombre de picto)
+    dead BOOLEAN,
+    ban BOOLEAN,
+    baseDef TINYINT,
+    x TINYINT,
+    y TINYINT,
+    job TINYINT,
+    mapWid TINYINT,
+    mapHei TINYINT,
+    mapDays TINYINT,
+    conspiracy BOOLEAN,
+    custom BOOLEAN,
+    PRIMARY KEY(user,dt),
+    FOREIGN KEY(user) REFERENCES user(id)
+);
+CREATE TABLE invitation (
+    user INT NOT NULL,
+    challenge INTEGER NOT NULL,
+    PRIMARY KEY(user,challenge),
+    FOREIGN KEY(challenge) REFERENCES challenge(id)
+);
---
+++ db/dummy_data.sql
@@ -0,0 +1,55 @@
+USE hordes_challenge;
+
+SET @open = 0;
+SET @request = 1;
+SET @invite = 2;
+
+SET @private = 1 << 2;
+
+SET @noapi = 1 << 3;
+
+SET @draft = 0 << 4;
+SET @review = 1 << 4;
+SET @invitation = 2 << 4;
+
+INSERT INTO user VALUES(1251, "Farijo", "farijo", null);
+INSERT INTO user VALUES(1252, "Rima", "rima", "https:\/\/myhordes.eu\/cdn\/avatars\/1252\/e545256c415fb25322c351946a1ec724.jpeg");
+INSERT INTO user VALUES(1253, "Odium", "odium", "https:\/\/myhordes.eu\/cdn\/avatars\/1253\/c79b9cdd2bd6e6989d7c70478f9c335c.png");
+INSERT INTO user VALUES(1254, "Loki", "loki", null);
+INSERT INTO user VALUES(1255, "LaCreme", "lacreme", "https:\/\/myhordes.eu\/cdn\/avatars\/1255\/9ab5b46b542ef1f3e0687faa51a180b7.jpeg");
+INSERT INTO user VALUES(1256, "kikess", "kikess", null);
+
+INSERT INTO challenge(name, creator, flags, start_date, end_date) VALUES("ch1", 1251, 0, NULL, NULL);
+INSERT INTO challenge(name, creator, flags, start_date, end_date) VALUES("ch1", 1251, 0, '2023-01-01', NULL);
+INSERT INTO challenge(name, creator, flags, start_date, end_date) VALUES("ch2", 1251, @request | @invitation, '2023-01-01', NULL);
+INSERT INTO challenge(name, creator, flags, start_date, end_date) VALUES("ch3", 1251, @request | @private | @invitation, '2023-01-01', NULL);
+INSERT INTO challenge(name, creator, flags, start_date, end_date) VALUES("ch3", 1252, @request | @private | @invitation, '2023-01-01', NULL);
+
+INSERT INTO participant VALUES(1252, 5);
+INSERT INTO participant VALUES(1252, 2);
+INSERT INTO participant VALUES(1252, 3);
+INSERT INTO participant VALUES(1252, 4);
+INSERT INTO participant VALUES(1253, 4);
+INSERT INTO participant VALUES(1253, 2);
+INSERT INTO participant VALUES(1253, 3);
+INSERT INTO participant VALUES(1254, 4);
+INSERT INTO participant VALUES(1254, 2);
+INSERT INTO participant VALUES(1254, 3);
+INSERT INTO participant VALUES(1255, 3);
+INSERT INTO participant VALUES(1255, 2);
+
+INSERT INTO validator VALUES(1251, 2);
+INSERT INTO validator VALUES(1251, 3);
+INSERT INTO validator VALUES(1251, 4);
+INSERT INTO validator VALUES(1255, 2);
+INSERT INTO validator VALUES(1255, 4);
+
+INSERT INTO goal(challenge, typ, descript) VALUES(2, 1, "::0");
+INSERT INTO goal(challenge, typ, descript) VALUES(2, 1, "::3");
+INSERT INTO goal(challenge, typ, descript) VALUES(2, 2, "::3");
+INSERT INTO goal(challenge, typ, descript) VALUES(3, 1, "::0");
+INSERT INTO goal(challenge, typ, descript) VALUES(3, 1, "::3");
+INSERT INTO goal(challenge, typ, descript) VALUES(3, 2, "::3");
+INSERT INTO goal(challenge, typ, descript) VALUES(4, 1, "::0");
+INSERT INTO goal(challenge, typ, descript) VALUES(4, 1, "::3");
+INSERT INTO goal(challenge, typ, descript) VALUES(4, 2, "::3");
