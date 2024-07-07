7df51664c58263aa6276d8ee322cf3e4f08103d3
Teddy <vallarteddy@yahoo.fr>
2024-07-07 13:08:11 +0200

    multiple translations

--- translator.py
+++ translator.py
@@ -1,28 +1,34 @@
 from string import Template
-from os.path import basename
+from os.path import basename, join
 import argparse
 import yaml
 
 parser = argparse.ArgumentParser(
                     prog=basename(__file__),
-                    description='Generate a translated file based on template template_file and translation file translation_file')
+                    description='Generate a translated file based on template_file for each TRANSLATION_FILE')
 parser.add_argument('template_file')
-parser.add_argument('translation_file')
-parser.add_argument('-o', '--output', help='Write to OUTPUT instead of stdout')
+parser.add_argument('-tr', '--translation-file', nargs="+", help='Yaml translation files to apply')
+parser.add_argument('-trname', '--translate-name', action='store_true', help='Also apply translation to filename')
+parser.add_argument('-o', '--output-dir', help='Directory to write generated files', default='.')
+parser.add_argument('-d', '--delimiter', help='Custom delimiter for template parsing', default='@@')
 args = parser.parse_args()
 
 class GoTemplate(Template):
-    delimiter = '@@'
+    delimiter = args.delimiter
     idpattern = '(?a:[\-_a-z][\-_a-z0-9]*)'
+
+outfile = basename(args.template_file)
 
 with open(args.template_file) as f:
     t = GoTemplate(f.read())
 
-with open(args.translation_file) as f:
-    y = yaml.safe_load(f)
+for lng in args.translation_file:
+    with open(lng) as f:
+        y = yaml.safe_load(f)
 
-if args.output:
-    with open(args.output, 'w') as f:
+    genfilename = outfile
+    if args.translate_name:
+        genfilename = GoTemplate(genfilename).substitute(y)
+
+    with open(join(args.output_dir, genfilename), 'w') as f:
         f.write(t.substitute(y))
-else:
-    print(t.substitute(y))
