63623b9634bba31313631d4940a9573ebfc4c8bc
Teddy <vallarteddy@yahoo.fr>
2024-07-09 15:41:39 +0200

    factorize

---
+++ script/date_format.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var scrollpos = localStorage.getItem('scrollpos');
+    if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function(e) {
+    localStorage.setItem('scrollpos', window.scrollY);
+};
+
+const endDateInput = $('[name=end_date]');
+
+function setupDateReactions(dateInput, oldDateTxt, oldDateVal, oldDateConfirm, newDateTxt) {
+    dateInput.on('change', function () {
+        if(!this.value || new Date(this.value) < new Date()) {
+            dateInput.siblings('button').text(oldDateTxt);
+            dateInput.siblings('button').val(oldDateVal);
+            dateInput.parent().attr('onsubmit', "return confirm(`" + oldDateConfirm + "`)");
+        } else {
+            dateInput.siblings('button').text(newDateTxt);
+            dateInput.siblings('button').val('validate');
+            dateInput.parent().attr('onsubmit', "dateToISOGMT(this)");
+        }
+    }).trigger('change');
+}
+
+function decomposeTemps(duree) {
+    return `~ ${Math.floor(duree / 3600).padStart(2, '0')}:${Math.floor((duree % 3600) / 60).padStart(2, '0')}:${(duree % 60).padStart(2, '0')}`;
+}
