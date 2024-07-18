cdd3360f69e01169fb214f09a8e6ab37fc6cb8bb
Teddy <vallarteddy@yahoo.fr>
2024-07-18 18:17:04 +0200

    load goals

---
+++ script/goals_lucha.js
@@ -0,0 +1,17 @@
+bindGoal({
+    Typ: 0,
+    Amount: {
+        Valid: true,
+        Int32: 98
+    },
+    Entity: 99
+});
+addAGoal(true);
+bindGoal({
+    Typ: 0,
+    Amount: {
+        Valid: true,
+        Int32: 205
+    },
+    Entity: 67
+});
