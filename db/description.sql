51258d08ebc23d3a0c05910ab931921164b5a9fd
Teddy Vallar <teddy.vallar@flowbird.group>
2024-08-09 11:56:54 +0200

    don't delete  milestone

--- db/description.sql
+++ db/description.sql
@@ -56,7 +56,7 @@ CREATE TABLE success (
     PRIMARY KEY(user,goal,amount),
     FOREIGN KEY(goal) REFERENCES goal(id),
     FOREIGN KEY(user) REFERENCES user(id),
-    FOREIGN KEY(user,accomplished) REFERENCES milestone(user,dt) ON DELETE CASCADE
+    FOREIGN KEY(user,accomplished) REFERENCES milestone(user,dt)
 );
 CREATE TABLE milestone (
     user INT NOT NULL,
